c38d4cb49803bd72b7221c6ed8fa908dcc0c7c79
root <vardhan.jay@gmail.com>
2019-10-22 11:45:56 +0530

    added user avatar in comment

--- src/templates/tmpl-comment-box.php
+++ src/templates/tmpl-comment-box.php
@@ -1,8 +1,7 @@
-<div class="orbit-oec-wrapper">
+<div class="row">
+	<div class="orbit-oec-wrapper" >
 	<div class="colo-sm-12 text-center">
-		<h4>Editor's Comment</h4>
-		<h5 class="muted">Post Your Querie's to YKA Editorial Team</h5>
-
+		<h4>Write to Youth Ki Awaaz Editors</h4>	
 	</div>
 	<div class="col-sm-12">
 		<ul class="orbit-oec-comments">
@@ -16,8 +15,13 @@
 								<?php _e(stripslashes($comment['comment'])); ?>
 
 								<div class="comment-meta">
-									<a href="<?php _e( get_author_posts_url( $user->ID )); ?>">
-										<?php _e($user->display_name); ?>
+									<a href="<?php _e( get_author_posts_url( $user->ID )); ?>"> <?php
+										if(function_exists('yka_get_avatar')){ 
+											echo yka_get_avatar($user->ID, 30);
+											echo "&nbsp;&nbsp;";    
+										}  
+										_e($user->display_name); 
+									?>
 									</a> on <?php _e($comment['commented_on'])?>
 								</div>
 							</div>
@@ -35,4 +39,4 @@
 		</form>
 	</div>
 </div>
-
+</div>
