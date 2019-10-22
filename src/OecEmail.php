f2059cf1afc35d984d7ec070010ad249b368454d
root <vardhan.jay@gmail.com>
2019-10-22 15:33:39 +0530

    reverting back

--- src/OecEmail.php
+++ src/OecEmail.php
@@ -12,17 +12,17 @@ class OecEmail extends Singleton
 	 * action hook callback function
 	 *
 	 **/
-	/*function oec_cron_mail($to, $subject, $body, $header)
+	function oec_cron_mail($to, $subject, $body, $header)
 	{
 		wp_mail($to, $subject, $body, $header);
-	}*/
+	}
 	
 	/**
 	 * 
 	 * Use this function to schedule email. 
 	 *
 	 **/
-	function scheduleEmail($to, $subject, $body, $header)
+	/*function scheduleEmail($to, $subject, $body, $header)
 	{
 		$args = array(
 				'to'		=> $to,
@@ -34,26 +34,36 @@ class OecEmail extends Singleton
 		if(!wp_next_scheduled( 'oec_cron_mail' )){
 			wp_schedule_single_event( time(), 'oec_cron_mail', $args );
 		}
-	}
+	}*/
 
 	
 	function emailNotification($userID, $postID, $comment) 
 	{
+		$postTitle   = get_the_title($postID); 
 		$authorID 	 = get_post_field( 'post_author', $postID );
 		$authorEmail = get_the_author_meta('user_email', $authorID);
 
 		if($userID == $authorID) {
 			$to = "jaydroid007@gmail.com";
 			//$to = "editor@youthkiawaaz.com";
+			$name = "Admin";
 		} else {
 			$to = $authorEmail;
+			$name = get_the_author_meta('display_name', $authorID);
 		}
 
+
 		$subject = 'Editor\'s Comment: New Message';
-		$body 	 = $comment;
+		$body 	 = "Hi $name,<br/>Your Article: $title has recieved following feedback message:<br> $comment";
 		$headers = array('Content-Type: text/html; charset=UTF-8');
 
-		$this->scheduleEmail( $to, $subject, $body, $headers );
+		//$this->scheduleEmail( $to, $subject, $body, $headers );
+
+		if(function_exists('yka_mail')) {
+			yka_mail( $to, $subject, $body, $headers );
+		} else {
+			wp_mail( $to, $subject, $body, $headers );
+		}
 	}
 
 }
